2c0ab2bd401a6eedf3c9c83a74af0ce284d4c9bb
IDStack <idstack@IDStacks-MacBook-Pro.local>
2020-10-11 20:18:50 +0700

    Membuat Chart di Laravel

--- app/Attendance.php
+++ app/Attendance.php
@@ -2,11 +2,18 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
     protected $guarded = [];
+
+    public function scopeCountAttendance($query, $status)
+    {
+        return $query->whereDate('created_at', Carbon::today())
+            ->where('status', $status)->count();
+    }
 
     public function detail()
     {
