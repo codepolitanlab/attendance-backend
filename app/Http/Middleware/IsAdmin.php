40219e16838ea3897f30ad3ac8312634b9b60dcb
IDStack <idstack@IDStacks-MacBook-Pro.local>
2020-10-11 22:44:34 +0700

    Implementasi Middleware Untuk Admin

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (! $request->user()->is_admin) {
+            return redirect('/home');
+        }
+
+        return $next($request);
+    }
+}
